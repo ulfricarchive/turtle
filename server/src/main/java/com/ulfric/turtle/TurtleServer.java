7a44cf31ef8e1e944609c16433d8300f3285c051
insou22 <zac.kologlu@gmail.com>
2017-01-29 18:04:15 +1100

    SERVER: Refactor to instance-bound server

--- server/src/main/java/com/ulfric/turtle/TurtleServer.java
+++ server/src/main/java/com/ulfric/turtle/TurtleServer.java
@@ -4,21 +4,36 @@ import java.security.NoSuchAlgorithmException;
 
 import javax.net.ssl.SSLContext;
 
+import com.ulfric.commons.exception.Try;
+
 import io.undertow.Undertow;
 import io.undertow.util.Headers;
 
-public class TurtleServer {
+public final class TurtleServer {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException
 	{
-		Undertow server = Undertow.builder()
-				.addHttpsListener(8080, "localhost", SSLContext.getDefault())
+		TurtleServer server = new TurtleServer();
+		server.start();
+	}
+
+	private final Undertow undertow;
+
+	private TurtleServer() {
+		this.undertow = this.build();
+	}
+
+	private Undertow build() {
+		return Undertow.builder()
+				.addHttpsListener(8080, "localhost", Try.to(SSLContext::getDefault))
 				.setHandler(exchange -> {
 					exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
 				})
 				.build();
+	}
 
-		server.start();
+	private void start() {
+		this.undertow.start();
 	}
 
 }
