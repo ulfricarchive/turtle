8b0487e4299d37571690fb76b7b87e32dd706e3e
insou22 <zac.kologlu@gmail.com>
2017-01-29 17:52:50 +1100

    SERVER: Default Undertow server creation

--- server/src/main/java/com/ulfric/turtle/TurtleServer.java
+++ server/src/main/java/com/ulfric/turtle/TurtleServer.java
@@ -1,10 +1,24 @@
 package com.ulfric.turtle;
 
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
 public class TurtleServer {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws NoSuchAlgorithmException
 	{
-		
+		Undertow server = Undertow.builder()
+				.addHttpsListener(8080, "localhost", SSLContext.getDefault())
+				.setHandler(exchange -> {
+					exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+				})
+				.build();
+
+		server.start();
 	}
 
 }
