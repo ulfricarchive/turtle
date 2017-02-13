c5a6ffc2576b7f76f2c8c8c7e3673e0f7f91f1be
insou22 <zac.kologlu@gmail.com>
2017-02-13 19:28:34 +1100

    ORCHESTRATION: Refactor to be service

--- orchestration/src/main/java/com/ulfric/turtle/Orchestration.java
+++ orchestration/src/main/java/com/ulfric/turtle/Orchestration.java
@@ -2,17 +2,8 @@ package com.ulfric.turtle;
 
 import com.ulfric.commons.cdi.ObjectFactory;
 import com.ulfric.commons.cdi.inject.Inject;
-import com.ulfric.commons.cdi.scope.Shared;
 
-@Shared
-public class Orchestration {
-
-	public static void main(String[] args)
-	{
-		ObjectFactory factory = ObjectFactory.newInstance();
-		Orchestration orchestration = factory.requestExact(Orchestration.class);
-		orchestration.start();
-	}
+public class Orchestration extends TurtleService {
 
 	@Inject private ObjectFactory factory;
 
