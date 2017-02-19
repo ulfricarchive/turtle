ce08fe6345a8c52b78fe9ed9840cb19661b9ed99
insou22 <zac.kologlu@gmail.com>
2017-02-19 15:25:16 +1100

    Make orchestration a standalone application

--- orchestration/src/main/java/com/ulfric/turtle/Orchestration.java
+++ orchestration/src/main/java/com/ulfric/turtle/Orchestration.java
@@ -1,13 +1,21 @@
 package com.ulfric.turtle;
 
 import com.ulfric.commons.cdi.ObjectFactory;
+import com.ulfric.commons.cdi.initialize.Initialize;
 import com.ulfric.commons.cdi.inject.Inject;
 
-public class Orchestration extends TurtleService {
+public class Orchestration {
+
+	public static void main(String[] args)
+	{
+		ObjectFactory factory = ObjectFactory.newInstance();
+		factory.request(Orchestration.class);
+	}
 
 	@Inject private ObjectFactory factory;
 
-	private void start()
+	@Initialize
+	private void init()
 	{
 
 	}
